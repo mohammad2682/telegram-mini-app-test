4e9f9d1ff6c04c2f89166211357c916502fb63f4
mohammad2682 <mohammad.akradi96@gmail.com>
2024-06-09 11:30:06 +0330

    bot updated

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,7 +1,23 @@
 const { Telegraf } = require('telegraf')
 
 const TOKEN = "7369500154:AAFYQ4M3jV2ezs3JIYRlXnInN1kKuY08wDQ";
+const webAppLink = 'https://mohammad2682.github.io/telegram-mini-app-test/';
 
-const bot = new Telegraf(TOKEN)
-bot.start((ctx) => ctx.reply('Welcome To Test Mini App'))
-bot.launch()
+const bot = new Telegraf(TOKEN);
+
+bot.start((ctx) => {
+    ctx.reply('Welcome To Test Mini App ;)', {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: 'Open Web App',
+                        web_app: { url: webAppLink }
+                    }
+                ]
+            ]
+        }
+    });
+});
+
+bot.launch();
