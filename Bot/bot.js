c1b9e5176ff7330f494265b77b74d2033875ac85
mohammad2682 <mohammad.akradi96@gmail.com>
2024-06-09 11:10:29 +0330

    app done

--- Bot/bot.js
+++ Bot/bot.js
@@ -3,5 +3,5 @@ const { Telegraf } = require('telegraf')
 const TOKEN = "7369500154:AAFYQ4M3jV2ezs3JIYRlXnInN1kKuY08wDQ";
 
 const bot = new Telegraf(TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
+bot.start((ctx) => ctx.reply('Welcome To Test Mini App'))
 bot.launch()
